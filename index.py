1d7980427f27c0d7081093f71e91109a51737408
Wh1te-Crow <realsydak@gmail.com>
2018-10-12 04:31:29 +0300

    Version 1.2 with match indices

--- index.py
+++ index.py
@@ -30,9 +30,27 @@ def check(first,second):
 				return True
 	return False
 
+def search_index_matching(Our_string,key_length):
+	temp_string = ""
+	for i in range(0,len(Our_string),key_length):
+		temp_string+=Our_string[i]
+	temp_dict = {}
+	for i in alphabet:
+		temp_dict[i]=0
+	for i in alphabet:
+		for j in temp_string:
+			if i == j:
+				temp_dict[i]+=1
+	index_matching = 0
+	for i in temp_dict.keys():
+		index_matching+=(temp_dict[i])*(temp_dict[i]-1)/(len(temp_string)*(len(temp_string)-1))
+	return index_matching
+
 create_table(vigenere_table)
-print_table(vigenere_table)
+#print_table(vigenere_table)
 #creating keys
+index_dict ={}
+
 keys = {}
 for i in [2,3,4,5,10,11,12,13,14,15,16,17,18,19,20]:
 	keys[i]=""
@@ -47,14 +65,18 @@ for i in [2,3,4,5,10,11,12,13,14,15,16,17,18,19,20]:
 		
 file = open('our-text.txt', encoding='utf-8')
 base_str = file.read()
-
+index_dict['open']=search_index_matching(base_str,1)
 
 for i in keys.keys():
-	cipher_text = open('cipher_texts/cipher_text['+str(i)+'].txt', 'w')
+	cipher_text = open('cipher_texts/cipher_text[{}].txt'.format(str(i)), 'w')
 	temp_str = ""		
 	for Index in range(len(base_str)):
 		temp_str += vigenere_table[base_str[Index]+keys[i][Index%len(keys[i])]]
+	index_dict[str(i)]=search_index_matching(temp_str,i)
 	cipher_text.write(temp_str)
 	cipher_text.close()
 
+print('<---match indices--->')
+for i in index_dict.keys():
+	print('[{}]:{}'.format(i,str(index_dict[i])))
 file.close()		
